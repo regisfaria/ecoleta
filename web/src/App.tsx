efaf648f883a8f24e850a70de85a703c059ece2b
regisfaria <rgisfaria@yahoo.com.br>
2020-06-06 17:50:42 -0300

    Clean project upload

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,22 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './App.css'
 
-import Header from './Header'
-
 function App() {
-  const [counter, setCounter] = useState(0)     //This returns a array [value, updateValue]
-
-  function handleButtonClick() {
-    setCounter(counter+1)
-  }
-  
   return (
-    <div>
-      <Header title="Ecoleta" />
-
-      <h1>{counter}</h1>
-      <button type='button' onClick={handleButtonClick}>Raise</button>
-    </div>
+    <h1>Hello World</h1>
   )
 }
 
